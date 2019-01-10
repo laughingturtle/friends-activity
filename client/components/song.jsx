bf51ef0b3d33066128c73bdd0e3f68fe4109c4a7
Malcolm Jackson <mj@deepplanet.com>
2019-01-09 22:43:20 -0800

    added React Styled-Components css

--- client/components/song.jsx
+++ client/components/song.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
 import moment from 'moment';
 
+const SongBlock = styled.div`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 270px; 
+  cursor: pointer;
+`;
+
+const CircleImg = styled.img`
+  border-radius: 50%;
+  margin-right: 10px;
+  height: 50px;
+  width: 50px;
+  border: 2px solid #b3b3b3;
+`;
+
+const LeftBlock = styled.div`
+  padding-top: 20px;
+  float:left; 
+  clear:left;
+`;
+
+const RightBlock = styled.div`
+  padding-top: 20px;
+  float:left; 
+`;
+
 var SongListEntry = ({song, handleSongClick}) => (
-      <div className="songBlock" onClick={() => handleSongClick(song.song_id)}>
-        <div className="leftBlock"><img className="circMe" src={song.img}/></div>
-        <div className="rightBlock friendName"><strong>{song.first_name} {song.last_name}</strong>
-          
-          <div className="songName details"> <FontAwesomeIcon icon="play-circle" id="white"/> {song.song_name}</div>
-          <div className="songArtist details">{song.artist}</div>
-          <div className="datePlayed details">{moment(song.date_last_played).startOf('day').fromNow()}</div>
-        </div>
-      </div> 
+      <SongBlock onClick={() => handleSongClick(song.song_id)}>
+        <LeftBlock><CircleImg src={song.img}/></LeftBlock>
+        <RightBlock className="friendName"><strong>{song.first_name} {song.last_name}</strong>   
+          <div className="details"> <FontAwesomeIcon icon="play-circle" id="white"/> {song.song_name}</div>
+          <div className="details">{song.artist}</div>
+          <div className="details small">{moment(song.date_last_played).startOf('day').fromNow()}</div>
+        </RightBlock>
+      </SongBlock> 
   );
 
   // `var` declarations will only exist globally where explicitly defined
